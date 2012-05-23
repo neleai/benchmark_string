3d8db0c5bac2a24e87838ee67382d04b6a263dfb
Ondrej Bilka <neleai@seznam.cz>
2012-05-23 14:46:58 +0200

    factored tester script

--- memmem/memmem_glibc.c
+++ memmem/memmem_glibc.c
@@ -1,3 +1,6 @@
+#define _GNU_SOURCE
+#include <string.h>
+
 char *memmem2(char *s,int slen,char *n,int nlen)
 {
   return memmem(s,slen,n,nlen);
--- tester.c
+++ tester.c
@@ -1,3 +1,4 @@
+
 #define _GNU_SOURCE
 #include <stdint.h>
 #include <math.h>
@@ -29,15 +30,15 @@ __inline__ uint64_t rdtsc(void)
   __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
   return (uint64_t)hi << 32 | lo;
 }
-char *strstr2(char *s,char *n);
 char *needle2,*haystack2;
 char *needle,*haystack;
 double fhlen;
+int r_seed;
 void random2(char *s,int len)
 {
   int j;
   for(j=0; j<len; j++)
-    s[j]=rand()%128+1;
+    s[j]=rand_r(&r_seed)%128+1;
 }
 void aaab(char *s,int len)
 {
@@ -56,10 +57,10 @@ void planted(char *haystack,int haystack_len)
   char next=0;
   while(j<haystack_len)
     {
-      int r=rand()&((1<<20)-1);
+      int r=rand_r(&r_seed)&((1<<20)-1);
       if (!plant[r])
         {
-          haystack[j]=rand()%128+1;
+          haystack[j]=rand_r(&r_seed)%128+1;
           if (haystack[j]!=needle[0] && haystack[j]!=next)
             {
               next=0;
@@ -83,7 +84,6 @@ void test(int runs,int needle_len,void (*needle_fn)(),int haystack_len, void (*h
 {
   if (runs<100) runs=100;
   double *times=malloc(runs*sizeof(double));
-  int sos=0;// to check if implementations give identical output
   int i;
   int j,k;
   uint64_t time_start,time_end,time_mid;
@@ -101,30 +101,35 @@ void test(int runs,int needle_len,void (*needle_fn)(),int haystack_len, void (*h
         }
       else
         {
-          needle=needle2+(rand()%512);
-          haystack=haystack2+(rand()%512);
+          needle=needle2+(rand_r(&r_seed)%512);
+          haystack=haystack2+(rand_r(&r_seed)%512);
         }
-
+      int cur_seed=r_seed;
       needle_fn(  needle  ,needle_len);
       needle[needle_len]=0;
       haystack_fn(haystack,haystack_len);
       haystack[haystack_len]=0;
-      if(haystack_len>needle_len)
+      if(haystack_len>=needle_len)
         {
           for(j=0; j<needle_len; j++)
             haystack[haystack_len-needle_len+j]=needle[j];
         }
 
+
       time_start=rdtsc();
-      char *r=strstr2(haystack,needle);
-      if(r) sos+=r-haystack;
+      char *r=TEST_FN;
       time_end=rdtsc();
-
+      if ((needle_len> haystack_len&&r) ||
+          (needle_len<=haystack_len&&r!=haystack+haystack_len-needle_len)){
+        printf("Wrong answer. Used seed %i",cur_seed);
+        exit(-3);
+      } 
       times[i]=time_end-time_start-ts_avg;
-
       /*ignore resheduled*/
       if (times[i]<-1000)               times[i]=times[i-1];
       if (times[i]>1000.0*haystack_len) times[i]=times[i-1];
+
+
     }
   double mean=0;
   for(i=0; i<runs; i++) mean+=times[i];
@@ -135,7 +140,7 @@ void test(int runs,int needle_len,void (*needle_fn)(),int haystack_len, void (*h
   fprintf(plot,"%f %f\n" ,fhlen,mean);
   fprintf(plot_r,"%f %f\n",fhlen,mean/haystack_len);
 
-  fprintf(stderr,"%i %i %i\n",needle_len,haystack_len,sos);
+  fprintf(stderr,"%i %i\n",needle_len,haystack_len);
   free(times);
 }
 int plant_r[100],plant_n[100];
@@ -143,7 +148,30 @@ void *get_gen(char *s)
 {
   if (!strcmp(s,"random")) return random2;
   if (!strcmp(s,"aaab"  )) return aaab;
-  if (!strcmp(s,"planted" )) return planted;
+  if (!strcmp(s,"planted" )){long sum; int i,j;
+    if (RAND_MAX<(16<<20)) { 
+      printf("RAND_MAX too small"); exit(-2);
+    }
+    FILE *f=fopen("plant","r");
+      plant_no=1<<20;
+      sum=0;
+      int no;
+      for (no=0; fscanf(f,"%i %i",plant_r+no,plant_n+no)!=EOF; no++)
+        {
+          sum+=1000*plant_r[no]/(plant_n[no] ? plant_n[no] : 1);
+        }
+      int avail=0;
+      for (i=0; i<no; i++)
+        {
+          int ratio=plant_no/sum*(1000*plant_r[i]/(plant_n[i] ? plant_n[i] : 1));
+          if (avail+ratio>=plant_no) break;
+          for (j=0; j<ratio; j++) plant[avail+j]=plant_n[i];
+          avail+=ratio;
+        }
+      for(j=avail; j<plant_no; j++) plant[j]=0;
+
+   return planted;
+  }
 }
 int main(int argc,char **argv)
 {
@@ -162,27 +190,9 @@ int main(int argc,char **argv)
   int  nlen = atoi(argv[4]);
   needle2  =malloc(nlen+1000);
   haystack2=malloc(20*slen+1000);
-  if (haystack_gen==planted)
     {
-      FILE *f=fopen("plant","r");
-      plant_no=1<<20;
-      sum=0;
-      int no;
-      for (no=0; fscanf(f,"%i %i",plant_r+no,plant_n+no)!=EOF; no++)
-        {
-          sum+=1000*plant_r[no]/(plant_n[no] ? plant_n[no] : 1);
         }
-      int avail=0;
-      for (i=0; i<no; i++)
-        {
-          int ratio=plant_no/sum*(1000*plant_r[i]/(plant_n[i] ? plant_n[i] : 1));
-          if (avail+ratio>=plant_no) break;
-          for (j=0; j<ratio; j++) plant[avail+j]=plant_n[i];
-          avail+=ratio;
-        }
-      for(j=avail; j<plant_no; j++) plant[j]=0;
-    }
-  for(i=2; i<20; i++)
+  for(i=2; i<=20; i++)
     {
       fhlen=(slen*i)/2.0;
       test(100000/(slen==1 ? 10 : slen),nlen,needle_gen,(slen*i)/2,haystack_gen);
