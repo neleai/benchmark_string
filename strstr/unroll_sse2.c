50c62d60023accf01ac15664fcc6ae33719833f8
Ondrej Bilka <neleai@seznam.cz>
2012-06-21 00:48:22 +0200

    renamed benchmarks

--- strstr/unroll_sse2.c
+++ strstr/unroll_sse2.c
@@ -3,7 +3,7 @@ const int unroll   = 4
 ,         phase1   = 2
 ,         phase2   = 8
 ,         prefetch = 4
-,         treshold = 512
+,         treshold = 128
 ;
 int reversed(){return 1;}
 #define SSE2_ONLY
