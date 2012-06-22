8e53af56718fc5004a6178f7aed64b821ab5a77a
Ondrej Bilka <neleai@seznam.cz>
2012-06-22 14:10:14 +0200

    strlen

--- memmem/cases/tests.h
+++ memmem/cases/tests.h
@@ -9,11 +9,11 @@ int ns,ss; int cur_seed;
 int r_seed;
 
 
-char *strstr2(char *a,int ss,char *c,int ns);
+char *memmem2(char *a,int ss,char *c,int ns);
 char *strstr3(char *a,char *b){
 
   bench_start();
-  char *r=strstr2(a,ss,b,ns);
+  char *r=memmem2(a,ss,b,ns);
   bench_end(ss);
   if (CHECK_TEST) {
     fprintf(stderr,"invalid match. Call try_test(%i,%i,%i) to reproduce.\n",ns,ss,cur_seed); 
