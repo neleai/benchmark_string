a8672660eb64b6d6154d9f11ddf4f411a8c3c068
Ondrej Bilka <neleai@seznam.cz>
2012-09-29 11:35:14 +0200

    lower bounds

---
+++ strlen/lower_bound/lower_bound_nta.s
@@ -0,0 +1,49 @@
+# orange
+	.file	"test_glibc.c"
+	.text
+	.p2align 4,,15
+.globl reversed
+	.type	reversed, @function
+reversed:
+.LFB12:
+	.cfi_startproc
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE12:
+	.size	reversed, .-reversed
+	.p2align 4,,15
+.globl strstr2
+	.type	strstr2, @function
+strstr2:
+.LFB13:
+	.cfi_startproc
+	movq	%rdi, %rax
+	movslq	%edx, %rdx
+	andq	$-64, %rax
+	addq	%rdx, %rdi
+	cmpq	%rdi, %rax
+	jae	.L3
+	jmp	.L6
+	.p2align 4,,10
+	.p2align 3
+.L6:
+  movntdqa (%rax),%xmm0
+  movntdqa 16(%rax),%xmm0
+  movntdqa 32(%rax),%xmm0
+  movntdqa 48(%rax),%xmm0
+	addq	$64, %rax
+	cmpq	%rdi, %rax
+	jb	.L6
+.L3:
+	rep
+	ret
+.L10:
+	.p2align 4,,4
+	rep
+	ret
+	.cfi_endproc
+.LFE13:
+	.size	strstr2, .-strstr2
+	.ident	"GCC: (Debian 4.5.3-12) 4.5.3"
+	.section	.note.GNU-stack,"",@progbits
---
+++ strlen/lower_bound_prefetch.s
@@ -0,0 +1,50 @@
+# gray
+	.file	"test_glibc.c"
+	.text
+	.p2align 4,,15
+.globl reversed
+	.type	reversed, @function
+reversed:
+.LFB12:
+	.cfi_startproc
+	xorl	%eax, %eax
+	ret
+	.cfi_endproc
+.LFE12:
+	.size	reversed, .-reversed
+	.p2align 4,,15
+.globl strstr2
+	.type	strstr2, @function
+strstr2:
+.LFB13:
+	.cfi_startproc
+	movq	%rdi, %rax
+	movslq	%edx, %rdx
+	andq	$-64, %rax
+	addq	%rdx, %rdi
+	cmpq	%rdi, %rax
+	jae	.L3
+	jmp	.L6
+	.p2align 4,,10
+	.p2align 3
+.L6:
+  prefetcht0 512(%rax)
+  movdqa (%rax),%xmm0
+  movdqa 16(%rax),%xmm0
+  movdqa 32(%rax),%xmm0
+  movdqa 48(%rax),%xmm0
+	addq	$64, %rax
+	cmpq	%rdi, %rax
+	jb	.L6
+.L3:
+	rep
+	ret
+.L10:
+	.p2align 4,,4
+	rep
+	ret
+	.cfi_endproc
+.LFE13:
+	.size	strstr2, .-strstr2
+	.ident	"GCC: (Debian 4.5.3-12) 4.5.3"
+	.section	.note.GNU-stack,"",@progbits
