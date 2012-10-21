b3b9cc192f4a70b0e8984490c304c3c71d0dc382
Ondrej Bilka <neleai@seznam.cz>
2012-10-21 09:07:59 +0200

    updated hook

--- strlen/hook/strlen_hook.c
+++ strlen/hook/strlen_hook.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <sys/file.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 
 static __inline__ uint64_t rdtsc(void)
 {
@@ -16,40 +19,46 @@ static __inline__ uint64_t rdtsc(void)
 }
 
 
-static uint64_t near=0,far=0;
 static uint64_t freq[3][10];
-
+static uint64_t delay[64];
+__attribute__((constructor)) static void init(){int i,j;
+  for(i=0;i<3;i++) for(j=0;j<9;j++) freq[i][j]=0;
+  for(i=0;i<64;i++) delay[i]=0;
+}
 __attribute__((destructor)) static void save_cnt(){ int i,j;
   char *fname = "/var/log/strlen.prof";
-  FILE *fi = fopen(fname,"r");
-  uint64_t near2,far2,freq2;
+  FILE *fi = fopen(fname,"r+");
+  uint64_t freq2;
   if (fi){
     flock(fileno(fi),LOCK_EX);
-    if(fscanf(fi,"%lli %lli", &near2,&far2)){
-      near+=near2;far+=far2;
-    }
-    for(i=0;i<3;i++){
+      for(i=0;i<3;i++){
       for(j=0;j<9;j++){
-        if(fscanf(fi,"%lli ", &freq2)){
+        if(fscanf(fi,"%lli ", &freq2)>0){
           freq[i][j]+=freq2;
         }
       }
     }
-    flock(fileno(fi),LOCK_UN);
-    fclose(fi);
-  }
-  FILE *fo = fopen(fname,"w");
-  if (fo){
-    flock(fileno(fi),LOCK_EX);
-    fprintf(fo,"%i %i\n",near,far);
+    for(i=0;i<64;i++){
+        if(fscanf(fi,"%lli ", &freq2)>0){
+          delay[i]+=freq2;
+        }
+      }
+
+    ftruncate(fileno(fi),0);
+     fseek(fi, 0L, SEEK_SET);
     for(i=0;i<3;i++){
       for(j=0;j<9;j++){
-        fprintf(fo,"%lli  ", freq[i][j]);
+        fprintf(fi,"%lli  ", freq[i][j]);
       }
-      fprintf(fo,"\n");
+      fprintf(fi,"\n");
     }
+    for(i=0;i<64;i++){
+      fprintf(fi,"%lli ",delay[i]);
+    }
+
+//    fsync(fileno(fi));
     flock(fileno(fi),LOCK_UN);
-    fclose(fo);
+    fclose(fi);
   }
 }
 size_t strlen2(char *x){
@@ -62,8 +71,8 @@ static uint64_t last;
 size_t strlen(char *x){
   size_t r=strlen2(x);
   uint64_t ts=rdtsc();
-  if (ts<last+1000) near++;
-  else far++;
+  if (ts!=last)
+    delay[63-__builtin_clzl(ts-last)]++;
   last=ts;
   int mag=0;
   size_t r2=r;
