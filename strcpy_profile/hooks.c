90e46b75ec3dfa0fdbe806e1e1d9ad9bd900e87e
Ondrej Bilka <neleai@seznam.cz>
2013-03-22 10:57:43 +0100

    new strcpy version

--- strcpy_profile/hooks.c
+++ strcpy_profile/hooks.c
@@ -7,13 +7,6 @@ char *strcpy2(char *x,const char *y,int end){
 	char * p=fn(x,y);
 	size_t r= p-x;
   COMMON_MEASURE(function);
-
-	int i;
-	for (i=0;y[i];i++) {
-		if (x[i]!=y[i]) { printf("disagree\n%s\n%s\n",x,y); abort(); }
-	}
-	if (x[i]) {printf("nonull\n"); abort(); }
-
   return end ? p : x;
 }
 char *strcpy(char *dest,const char *src){	return strcpy2(dest,src,0); }
--- strcpy_profile/variant/strcpy_new.c
+++ strcpy_profile/variant/strcpy_new.c
@@ -49,27 +49,28 @@ char *STRCPY (char *dest, char *src)
 char *strcpy_internal (char *dest, char *src, char *ret)
 #endif
 {
+
   char *s, *d;
   tp_vector vz = BROADCAST (0);
-  tp_vector v0, v1, v2, v3;
+  tp_vector  v0, v1, v2, v3, vold;
+  tp_vector  w0, w1, w2, w3;
+
   tp_mask m, m2;
   long i, no, offset;
-
-page_cont:
   ;
 	/* Test if reading next 64 bytes cross a page.  */
   if ((((uint64_t) src) & (4096 - 1)) > 4096 - 64)
     {
-      offset = ((uint64_t) src) % 64;
-      for (i = offset; i < 64; i++)
+      for (i = 0; i < 64; i++)
         {
-          dest[i - offset] = src[i - offset];
-          if (!src[i - offset]) return _STR_STP (ret, dest + i - offset);
+          dest[i] = src[i];
+          if (!src[i]) return _STR_STP (ret, dest + i);
         }
       goto aligned;
     }
 
   /* Handle strings upto 16 bytes first.  */
+page_cont:;
   v0 = LOADU (src);
   m = get_mask (EQ (v0, vz));
   if (m)
@@ -98,26 +99,30 @@ page_cont:
 
   STOREU (dest + no + 1 - 16, LOADU(src + no + 1 - 16));
 
-  if (no != 63 || src[63] == 0)
+  if (src[no] == 0)
     {
       return _STR_STP (ret, dest + no);
     }
 
 aligned:
   ;
-  /* Align loads to 64 bytes.  */
+ 
+
+#ifdef UNALIGNED
+ /* Align loads to 64 bytes.  */
   offset = ((uint64_t) src) % 64;
   dest = dest + 64 - offset;
   src = src + 64 - offset;
+
+
+  /* We use aligned loads and unaligned stores.
+     This is faster than unaligned load and aligned stores because
+     test for load crossing page is not neccessary.  */
   v0 = LOAD (src + 0);
   v1 = LOAD (src + 16);
   v2 = LOAD (src + 32);
   v3 = LOAD (src + 48);
 
-#ifdef UNALIGNED
-  /* We use aligned loads and unaligned stores.
-     This is faster than unaligned load and aligned stores because
-     test for load crossing page is not neccessary.  */
   while (1)
     {
       /* Test zero byte among 64 bytes and if is then handle it by header.  */
@@ -134,41 +139,51 @@ aligned:
       dest += 64;
     }
 #else
+ /* Align loads to 64 bytes.  */
+	src += 64;
+	dest+= 64;
 
-	STOREU (dest, v0);
+  v0 = LOADU (src + 0);
+  v1 = LOADU (src + 16);
+  v2 = LOADU (src + 32);
+  v3 = LOADU (src + 48);
+  if (get_mask (EQ (MIN (v0, MIN (v1, MIN (v2, v3))), vz))) goto page_cont;
+  STOREU (dest + 0, v0);
+  STOREU (dest + 16, v1);
+  STOREU (dest + 32, v2);
+  STOREU (dest + 48, v3);
+
+
+  offset = ((uint64_t) src) % 64;
+  dest = dest + 64 - offset;
+  src = src + 64 - offset;
+
+
+	vold = LOAD (src - 16);	
+	v0 = LOAD (src + 0);
+	v1 = LOAD (src + 16);
+	v2 = LOAD (src + 32);
+	v3 = LOAD (src + 48);
+
+
 	switch ((dest-src)%16){
-	case 0:
-	  while (1)
-    {
-      /* Test zero byte among 64 bytes and if is then handle it by header.  */
-      if ( get_mask (EQ (MIN (v0, MIN (v1, MIN (v2, v3))), vz))) goto page_cont;
-      STORE (dest + 0, v0);
-      v0 = LOAD (src + 64);
-      STORE (dest + 16, v1);
-      v1 = LOAD (src + 16 + 64);
-      STORE (dest + 32, v2);
-      v2 = LOAD (src + 32 + 64);
-      STORE (dest + 48, v3);
-      v3 = LOAD (src + 48 + 64);
-      src += 64;
-      dest += 64;
-    }
-	
-		#define UNALIGNED_COPY(align)\
-		 case align:\
+#define UNALIGNED_COPY(align)\
+		case align:\
     while(1){\
-      if (get_mask (EQ (MIN (v0, MIN (v1, MIN (v2, v3))), vz))) {goto end_page;}\
-			STORE (dest +16- align + 0, CONCAT(v0,v1,16-align));\
+      if ( get_mask (EQ (MIN (v0, MIN (v1, MIN (v2, v3))), vz))) {goto end_page;}\
+ 			STORE (dest - align + 0, CONCAT(vold,v0,16-align));\
+			STORE (dest - align + 16, CONCAT(v0,v1,16-align));\
       v0 = LOAD (src + 64);\
-			STORE (dest +16- align + 16, CONCAT(v1,v2,16-align));\
+			STORE (dest - align + 32, CONCAT(v1,v2,16-align));\
       v1 = LOAD (src + 16 + 64);\
-			STORE (dest +16- align + 32, CONCAT(v2,v3,16-align));\
+			STORE (dest - align + 48, CONCAT(v2,v3,16-align));\
       v2 = LOAD (src + 32 + 64);\
- 			STORE (dest +16- align + 48, CONCAT(v3,v0,16-align));\
+			vold = v3; \
 	    v3 = LOAD (src + 48 + 64);\
       src += 64;\
       dest += 64;\
     }
+		UNALIGNED_COPY(0);
 		UNALIGNED_COPY(1);
 		UNALIGNED_COPY(2);
 		UNALIGNED_COPY(3);
@@ -185,13 +200,10 @@ aligned:
 		UNALIGNED_COPY(14);
 		UNALIGNED_COPY(15);
 	}
+	end_page:
+	STOREU(dest-16, vold);
+	goto page_cont;
 #endif
-	end_page:;
-	while(*src) 
-   *dest++ = *src++;
-	*dest = 0;
-	return  _STR_STP(ret, dest);
-
 }
 
 /* Copies up to 16 bytes and returns ret.  */
